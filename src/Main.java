2482fcc32146d76a29e2120e75b8a7d44b73d18f
Rebekka Hubert <hubert@cl.uni-heidelberg.de>
2023-02-02 19:01:41 +0100

    add difficulty choice

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,30 @@
+import java.util.Scanner;
+
+import static java.lang.System.in;
+import static java.lang.System.out;
+
 public class Main {
     public static void main(String[] args) {
-        Game tic_tac_toe = new Game(3);
+        Scanner scanner = new Scanner(in);
+        boolean readable_int = false;
+        int difficulty = 0;
+        while (!readable_int) {
+            out.print("Choose a difficulty: \n1) easy\n2) medium\n3) difficult\nYour choice: ");
+            if (scanner.hasNextInt()) {
+                int read_number = scanner.nextInt();
+                if (read_number < 1 || read_number > 3) {
+                    out.println("Please enter a valid number.");
+                    continue;
+                }
+                difficulty = read_number;
+                readable_int = true;
+            }
+            else {
+                out.println("Please enter a valid number.");
+                scanner.nextLine();
+            }
+        }
+        Game tic_tac_toe = new Game(difficulty);
         tic_tac_toe.game_loop();
     }
 }
