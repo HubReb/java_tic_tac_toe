a9dc29b1442d0e27af9c241992450524efbc621b
Rebekka Hubert <hubert@cl.uni-heidelberg.de>
2023-02-03 14:06:10 +0100

    extract score keeper into a new class

---
+++ src/GameKeeper.java
@@ -0,0 +1,72 @@
+import java.text.MessageFormat;
+import java.util.Scanner;
+
+import static java.lang.System.in;
+import static java.lang.System.out;
+
+public class GameKeeper {
+    int difficulty;
+    int losses;
+    int victories;
+    int remis;
+    public GameKeeper() {
+        difficulty = 0;
+        losses = 0;
+        victories = 0;
+        remis = 0;
+    }
+    public void game() {
+        Scanner scanner = new Scanner(in);
+        boolean readable_int = false;
+
+        boolean play = true;
+        while (play) {
+            while (!readable_int) {
+                out.print("Choose a difficulty: \n1) easy\n2) medium\n3) difficult\nYour choice: ");
+                if (scanner.hasNextInt()) {
+                    int read_number = scanner.nextInt();
+                    if (read_number < 1 || read_number > 3) {
+                        out.println("Please enter a valid number.");
+                        continue;
+                    }
+                    this.difficulty = read_number;
+                    readable_int = true;
+                }
+                else {
+                    out.println("Please enter a valid number.");
+                    scanner.nextLine();
+                }
+            }
+            readable_int = false;
+            Game tic_tac_toe = new Game(difficulty);
+            int game_result = tic_tac_toe.game_loop();
+            switch (game_result) {
+                case Player.SIGN -> victories++;
+                case AI.SIGN -> losses++;
+                default -> remis++;
+            }
+            String score_message = MessageFormat.format(
+                    "Your current score:\nLosses: {0}\nVictories: {1}\nRemis: {2}", losses, victories, remis
+            );
+            out.println(score_message);
+            while (!readable_int) {
+                out.print("Start a new game?\n1) Yes\n2) No\nYour choice: ");
+                if (scanner.hasNextInt()) {
+                    int read_number = scanner.nextInt();
+                    if (read_number < 1 || read_number > 2) {
+                        out.println("Please enter a valid number.");
+                        continue;
+                    }
+                    if (read_number == 2) {
+                        play = false;
+                    }
+                    readable_int = true;
+                } else {
+                    out.println("Please enter a valid number.");
+                    scanner.nextLine();
+                }
+            }
+            readable_int = false;
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,65 +1,7 @@
-import java.text.MessageFormat;
-import java.util.Scanner;
-
-import static java.lang.System.in;
-import static java.lang.System.out;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(in);
-        boolean readable_int = false;
-        int difficulty = 0;
-        int losses = 0;
-        int victories = 0;
-        int remis = 0;
-        boolean play = true;
-        while (play) {
-            while (!readable_int) {
-                out.print("Choose a difficulty: \n1) easy\n2) medium\n3) difficult\nYour choice: ");
-                if (scanner.hasNextInt()) {
-                    int read_number = scanner.nextInt();
-                    if (read_number < 1 || read_number > 3) {
-                        out.println("Please enter a valid number.");
-                        continue;
-                    }
-                    difficulty = read_number;
-                    readable_int = true;
-                }
-                else {
-                    out.println("Please enter a valid number.");
-                    scanner.nextLine();
-                }
-            }
-            readable_int = false;
-            Game tic_tac_toe = new Game(difficulty);
-            int game_result = tic_tac_toe.game_loop();
-            switch (game_result) {
-                case Player.SIGN -> victories++;
-                case AI.SIGN -> losses++;
-                default -> remis++;
-            }
-            String score_message = MessageFormat.format(
-                    "Your current score:\nLosses: {0}\nVictories: {1}\nRemis: {2}", losses, victories, remis
-            );
-            out.println(score_message);
-            while (!readable_int) {
-                out.print("Start a new game?\n1) Yes\n2) No\nYour choice: ");
-                if (scanner.hasNextInt()) {
-                    int read_number = scanner.nextInt();
-                    if (read_number < 1 || read_number > 2) {
-                        out.println("Please enter a valid number.");
-                        continue;
-                    }
-                    if (read_number == 2) {
-                        play = false;
-                    }
-                    readable_int = true;
-                } else {
-                    out.println("Please enter a valid number.");
-                    scanner.nextLine();
-                }
-            }
-            readable_int = false;
-        }
+        GameKeeper keeper = new GameKeeper();
+        keeper.game();
     }
 }
